b328278a4458f57e34a255b38c446dbc36cf6989
mohammedmahdi548 <66646493+mohammedmahdi548@users.noreply.github.com>
2020-07-01 17:50:48 +0300

    solved

---
+++ Movie/app/src/main/java/com/example/movie/movie.java
@@ -0,0 +1,10 @@
+package com.example.movie;
+
+public class movie {
+    private String title ;
+    private String mainActor ;
+    private double movieRate ;
+    private int pgRate ;
+    private String genre ;
+
+}
